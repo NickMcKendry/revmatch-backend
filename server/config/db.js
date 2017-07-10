13f5bd6a7ff1990985d20c6a9f3e7e37f0d1e318
Nick McKendry <nickmckendry@gmail.com>
2017-07-10 14:21:50 -0600

    added start script

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export default () => {
   mongoose.Promise = global.Promise
-  mongoose.connect('process.env.mongodb://heroku_0l2lfc4h:4mo48a65tj778810m5irfjj5f2@ds155192.mlab.com:55192/heroku_0l2lfc4h')
+  mongoose.connect('mongodb://heroku_0l2lfc4h:4mo48a65tj778810m5irfjj5f2@ds155192.mlab.com:55192/heroku_0l2lfc4h')
   mongoose.connection
     .once('open', () => console.log('mongodb running'))
     .on('error', err => console.error(err))
