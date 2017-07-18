d6a53e35958a4f9169e69203cbeb10eef681ae2a
Nick McKendry <nickmckendry@gmail.com>
2017-07-17 23:09:51 -0600

    added config and passport initialized

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+const config = {
+  DB_URL: 'mongodb://heroku_0l2lfc4h:4mo48a65tj778810m5irfjj5f2@ds155192.mlab.com:55192/heroku_0l2lfc4h',
+  JWT_SECRET: 'a7sf0asfhfs7f312312321kksnfsfkgdadasfvbuw8712b2121d19bxi'
+}
+
+export default config
--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 
+import config from './config'
+
 export default () => {
   mongoose.Promise = global.Promise
-  mongoose.connect('mongodb://heroku_0l2lfc4h:4mo48a65tj778810m5irfjj5f2@ds155192.mlab.com:55192/heroku_0l2lfc4h')
+  mongoose.connect(config.DB_URL)
   mongoose.connection
     .once('open', () => console.log('mongodb running'))
     .on('error', err => console.error(err))
