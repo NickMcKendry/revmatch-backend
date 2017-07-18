8eac7a3076a97a96db0a815c53496a0b709b3dc0
Nick McKendry <nickmckendry@gmail.com>
2017-07-18 00:34:28 -0600

    testing facebook auth beginning

--- server/modules/users/model.js
+++ server/modules/users/model.js
@@ -2,8 +2,48 @@ import mongoose, { Schema } from 'mongoose'
 
 const UserSchema = new Schema({
   email: {
+    type: String,
+    unique: true
+  },
+
+  fullName: {
     type: String
+  },
+
+  avatar: {
+    type: String
+  },
+
+  car : {
+    type: String
+  },
+
+  providerData: {
+    uid: String,
+    provider: String
   }
-})
+},
+  {timestamps: true}
+)
+
+
+UserSchema.statics.findOrCreate = async function(args) {
+  try{
+    const user = await this.findOne({
+      email: args.email,
+      fullName: args.fullName
+    })
+
+    if(!user){
+      return await this.create(args)
+    }
+
+    return user
+  } catch(e) {
+    return e
+  }
+
+
+}
 
 export default mongoose.model('User', UserSchema)
